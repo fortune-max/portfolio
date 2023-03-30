abc31ae1027bb8fccb299f206ccd8542f230edb2
fortune-max <fortmax120@gmail.com>
2023-03-30 22:58:30 +0200

    update path in index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ import { Experience } from "~/components/Experience";
 import { Gallery } from "~/components/Gallery";
 import { Projects } from "~/components/Projects";
 import { Skills } from "~/components/Skills";
-import photos from "./photos.json";
+import photos from "../photos.json";
 
 export default function Index() {
   return (
