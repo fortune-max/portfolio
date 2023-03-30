ae8c87cbff3ec9884af9efa007cf2953fa59c1a4
fortune-max <fortmax120@gmail.com>
2023-03-30 22:56:25 +0200

    add index

---
+++ app/routes/index.tsx
@@ -0,0 +1,22 @@
+import { About } from "~/components/About";
+import { Awards } from "~/components/Awards";
+import { Experience } from "~/components/Experience";
+import { Gallery } from "~/components/Gallery";
+import { Projects } from "~/components/Projects";
+import { Skills } from "~/components/Skills";
+import photos from "./photos.json";
+
+export default function Index() {
+  return (
+    <div>
+      <About/>
+      <Skills />
+      <Experience />
+      <Awards />
+      <Projects />
+      <Gallery 
+        photos={photos}
+      />
+    </div>
+  );
+}
