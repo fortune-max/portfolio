ab237e53996db9c530268d1f3ff850a69874b9b9
fortune-max <fortmax120@gmail.com>
2023-03-31 22:24:23 +0200

    update About section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -7,6 +7,7 @@ export const About = () => {
                 <p>I enjoy writing beautiful code, and sometimes... building beautiful interfaces.</p>
                 <p>I’m a lot of things (including a Linux command :p) but at present, I am currently improving my frontend and design skills.</p>
                 <p>I enjoy reading (mostly sci-fi) in my spare time, playing vidya gaems, discovering memes, biking, and going on long walks.</p>
+                <p>PS: The background of the page is procedurally generated (<a href="http://mrl.nyu.edu/~perlin/noise/" className="hover:underline">sauce</a>) and changes on each refresh. This may make it less responsive on lower-powered mobile devices, but that's a tradeoff I can live with - it still looks hella cool 😎.</p>
             </div>
         </div>
     );
