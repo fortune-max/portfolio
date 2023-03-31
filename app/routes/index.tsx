ddb85cb39a7b1df7334885dcc7645e8d560a4cbc
fortune-max <fortmax120@gmail.com>
2023-03-31 22:05:56 +0200

    use hygraph in index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,15 +5,28 @@ import { Experience } from "~/components/Experience";
 import { Gallery } from "~/components/Gallery";
 import { Projects } from "~/components/Projects";
 import { Skills } from "~/components/Skills";
-import photos from "../photos.json";
-import workEntries from "../workEntries.json";
+import { sdk } from '~/libs/client';
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+  const { photos } = await sdk.Photos();
+  const { photos: nextPhotos } = await sdk.PhotosSecond({ skip: 10 });
+  photos.push(...nextPhotos);
+
+  const { workExperiences } = await sdk.WorkExperiences();
+
+  return json({ photos, workExperiences });
+}
 
 export default function Index() {
+  const { photos, workExperiences } = useLoaderData<typeof loader>();
+
   return (
     <div>
       <About/>
       <Skills />
-      <Experience workEntries={workEntries} />
+      <Experience workEntries={workExperiences} />
       <Awards />
       <Projects />
       <Gallery photos={photos} />
